b13e46961a6732864aa92d61bdeb86a350b2ed25
VolkovNikitaSerg <nikitavolkovserg@gmail.com>
2018-04-21 17:36:44 +0700

    Added firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+
+console.log("Worked");
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyC0vc8mZMEdMbXdbax1Cfd2q2YE-Dsu-OE",
+    authDomain: "expensify-f2504.firebaseapp.com",
+    databaseURL: "https://expensify-f2504.firebaseio.com",
+    projectId: "expensify-f2504",
+    storageBucket: "expensify-f2504.appspot.com",
+    messagingSenderId: "940034352305"
+};
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+    name: "Nikita Volkov",
+    age: "16"
+});
